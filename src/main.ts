890e7ad0d44c93edf83e1750935ad8eb5317179c
Tamara <tamarafrazzetta@gmail.com>
2024-08-27 20:22:21 -0300

    chore: add swagger and zod

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { patchNestjsSwagger } from '@anatine/zod-nestjs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { ConfigProps } from './config/config.types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   const configService = app.get(ConfigService);
   const globalPrefix = 'v1';
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Movies API')
+    .setDescription('API to manage Star Wars movies')
+    .setVersion('1.0')
+    .addTag('movies')
+    .build();
+  patchNestjsSwagger();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   const port = configService.get<ConfigProps['port']>('port');
   await app.listen(port).then(() => {
     console.log(`⚡​Listening at http://localhost:${port}/${globalPrefix}`);
