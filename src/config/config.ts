015c42c9d07d66fc0ce353688bde43d22e16c9eb
Tamara <tamarafrazzetta@gmail.com>
2024-08-28 23:03:21 -0300

    chore: rename app env var

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import { ConfigProps } from './config.types';
 
 export const config = (): ConfigProps => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.APP_PORT, 10) || 3000,
   database: {
     url: process.env.DATABASE_URL,
     name: process.env.DATABASE_NAME,
